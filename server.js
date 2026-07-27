afa5d042b18993e0deac10bdf7766b20d84bbd55
Gaurav Thakur <137675373+gauravthakur05@users.noreply.github.com>
2026-07-27 14:56:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const fs = require("fs");
 const path = require("path");
 const url = require("url");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Path to the compiled C++ program (differs on Windows vs Mac/Linux)
 const isWindows = process.platform === "win32";
